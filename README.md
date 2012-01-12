adf6f1ae31d6c3b883e7c0143bf2443bee119d98
Mike Kunze <michael.kunze@gmail.com>
2012-01-11 18:54:48 -0600

    added socket.io logic to update client instance.  sends emits to update clientsConsole

--- README.md
+++ README.md
@@ -47,7 +47,10 @@
     db.users.insert({ username: 'test1', userHash: '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014', password: '1b4f0e9851971998e732078544c96b36c3d01cedf7caa332359d6f1d83567014'})
     db.users.insert({ username: 'test2', userHash: '60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752', password: '60303ae22b998861bce3b28f33eec1be758a213c86c93c076dbe9f558c11c752'})
     db.users.insert({ username: 'test3', userHash: 'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13', password: 'fd61a03af4f77d870fc21e05e7e80678095c92d808cfb3b5c279ee04c74aca13'})
-    
+    db.users.insert({ username: 'test4', userHash: 'a4e624d686e03ed2767c0abd85c14426b0b1157d2ce81d27bb4fe4f6f01d688a', password: 'a4e624d686e03ed2767c0abd85c14426b0b1157d2ce81d27bb4fe4f6f01d688a'})
+    db.users.insert({ username: 'test5', userHash: 'a140c0c1eda2def2b830363ba362aa4d7d255c262960544821f556e16661b6ff', password: 'a140c0c1eda2def2b830363ba362aa4d7d255c262960544821f556e16661b6ff'})
+    db.users.insert({ username: 'test6', userHash: 'ed0cb90bdfa4f93981a7d03cff99213a86aa96a6cbcf89ec5e8889871f088727', password: 'ed0cb90bdfa4f93981a7d03cff99213a86aa96a6cbcf89ec5e8889871f088727'})
+
     You now have 4 test uses with the following user/pass combinations:
     test  / test
     test1 / test1
@@ -89,10 +92,10 @@ To witness current functionality, load the logConsole first.
     Ext.bang.util.getApp({ app: 'bang', js: 'broadcastMessage.js' }, function(err, app) {
       Ext.JSON.decode(app);
     });    
+    
+    
 ##### logMessage - API for server logger
-    Ext.bang.util.getApp({ app: 'bang', js: 'logMessage.js' }, function(err, app) {
-      Ext.JSON.decode(app);
-    });    
+    remotejs.logMessage('message', function() {});    
         
 #### remote execution
     There is the ability to push a client to run an application.  To do so, first
--- routes/ioStream.js
+++ routes/ioStream.js
@@ -1,19 +1,92 @@
 var ioStream = {};
-
-var mongoose,
-    logger,
-    io;
     
 ioStream.init = function(bang) {
-  mongoose = bang.mongoose;
-  logger   = bang.logger;
-  io       = bang.io;
+  ioStream.mongoose = bang.mongoose;
+  ioStream.logger   = bang.logger;
+  ioStream.io       = bang.io;
+  ioStream.security = bang.security;
   
   return ioStream;
 };
 
+ /**
+  * This is where we add our socket.io routes.
+  */ 
+ioStream.addRoutes = function(socket) {
+
+  ioStream.socket = socket;
+
+  console.log('socket_id: '  + socket.id + ' has connected');
+  socket.emit('connection received', {
+    timestamp: Date.now(),
+    socket_id: socket.id
+  });
+  
+  socket.on('update handshake',     ioStream.updateHandshake);
+  socket.on('client authenticated', ioStream.clientAuthenticated);    
+  socket.on('disconnect',           ioStream.disconnect);
+};
+
+ioStream.updateHandshake = function(data) {
+  var security = ioStream.security,
+      logger   = ioStream.logger,
+      mongoose = ioStream.mongoose,
+      crypto   = security.crypto;
+      
+  var hash = crypto.createHash('sha256');      
+  hash.update(data.handshake + Date.now() + data.user_id);
+  
+  var newHandshake = hash.digest('hex');
+    
+  var clients = mongoose.model('clients');
+  
+  var query = { 
+    socket_id: data.socket_id,
+    handshake: data.handshake 
+  }
+
+  console.log(query);
+
+  clients.findOne(query, function(err, doc) {
+    
+    console.log(doc);
+    if(doc) {
+      doc.handshake = newHandshake;
+      doc.timestamp = Date.now();
+      doc.socket_id = ioStream.socket.id
+      
+      doc.save(function(err) {
+        if(err) {
+          logger.logMessage(err, function() {});
+        }
+      });
+    } else {
+      var client = new clients({
+        handshake: newHandshake,
+        timestamp: Date.now(),
+        socket_id: ioStream.socket.id        
+      });
+      
+      client.save(function(err) {
+        if(err) {
+          console.log('failed to create new client');
+        }
+      });
+    }  
+    
+    ioStream.io.sockets.emit('newClient');
+    ioStream.socket.emit('update handshake', { 
+      handshake: newHandshake,
+      socket_id: ioStream.socket.id
+    });
+  });
+};
+
+/**
+ *
+ */
 ioStream.disconnect = function() {
-    var clients = mongoose.model('clients');
+    var clients = ioStream.mongoose.model('clients');
     clients.findOne({ socket_id: ioStream.socket.id }, function(err, doc) {
     
       if(err) {
@@ -24,37 +97,24 @@ ioStream.disconnect = function() {
         return;
       }
       
-      doc.remove();
-      io.sockets.emit('newClient', {});
-      console.log(ioStream.socket.id + '.sid disconnected ');
+      console.log(doc);
+      
+      // do a socket.on('connection received') client side
+      
+      
+      //doc.remove();
+      //io.sockets.emit('newClient', {});
+      //console.log(ioStream.socket.id + '.sid disconnected ');
     });
 };
 
 ioStream.clientAuthenticated = function(data) {
-    var clients = mongoose.model('clients');
+    var clients = ioStream.mongoose.model('clients');
     
     var client = new clients(data);
     client.save(function(err) {
-      io.sockets.emit('newClient', data);
+      ioStream.io.sockets.emit('newClient', data);
     });
-};
- 
- /**
-  * This is where we add our socket.io routes.
-  */ 
-ioStream.addRoutes = function(socket) {
-  // Reference our socket
-  ioStream.socket = socket;
-
-  console.log('socket_id: '  + socket.id + ' has connected');
-  socket.emit('connection received', {
-    timestamp: Date.now(),
-    socket_id: socket.id
-  });
-  
-  
-  socket.on('disconnect',           ioStream.disconnect);
-  socket.on('client authenticated', ioStream.clientAuthenticated);  
 };
 
 module.exports = ioStream;
