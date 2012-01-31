0e90fbab7184ecf41d4031b717644543061d8036
Mike Kunze <michael.kunze@gmail.com>
2012-01-30 21:24:54 -0600

    settings to coffee

--- server/settings/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  path: '/opt/bang.js',
-  debug: true,
-  web: require('./web.js'),
-  db:  require('./db.js')
-};
--- server/settings/web.js
+++
@@ -1,3 +0,0 @@
-exports.url      = 'http://bangjs';
-exports.port     = 3069;
-exports.redisKey = '289fjosijdf98';
